60ddf532ed4d236ea6a6c0ccf1c6dec348645cea
Ian Gosnell <33731381+IGOSNELL@users.noreply.github.com>
2021-02-21 14:12:38 +0200

    Add files via upload

--- server.py
+++ server.py
@@ -12,12 +12,12 @@ def my_home():
 def html_page(page_name):
      return render_template(page_name)
 
-def write_to_file(data):
-  with open('database.txt',mode='a') as database:
-     email= data['email']
-     subject = data['subject']
-     message = data['message']
-     file = database.write(f'\n{email},{subject},{message}')
+# def write_to_file(data):
+#   with open('database.txt',mode='a') as database:
+#      email= data['email']
+#      subject = data['subject']
+#      message = data['message']
+#      file = database.write(f'\n{email},{subject},{message}')
 
 def write_to_csv(data):
   with open('database.csv', 'a',newline='') as database2:
@@ -33,12 +33,15 @@ def write_to_csv(data):
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
+      try:
        data = request.form.to_dict()
        write_to_file(data)
        write_to_csv(data)
        return redirect('/thankyou.html')
+      except:
+        return 'did not save to database'
     else:
-       return 'something went wrong'
+       return 'something went wrong Try again'
 
     # render_template('login.html', error=error)
     # error = None
